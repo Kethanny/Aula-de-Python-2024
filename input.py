a1da3e12b27057f1685ac7a5cd6599b423a20529
Kéthanny Santa Rosa <kethynayane816@gmail.com>
2024-06-28 14:01:00 -0300

    media da nota

--- input.py
+++
@@ -1,9 +0,0 @@
-nome = input("Digite seu nome: ")
-idade = float(input("Digite seu idade: "))
-print(idade) 
-print(type(idade))
-idade_futura = idade + 3
-
-
-
- print(f"O nome da pessoa é {nome}, e a idade é {idade}, idade futura {idade_futura}")
