72f1217f89cdf8f2440a24a03159995031e4a36f
Kéthanny Santa Rosa <kethynayane816@gmail.com>
2024-06-28 13:10:47 -0300

    Desconto da Paula Martins

---
+++ aula1.py
@@ -0,0 +1,10 @@
+nome = "Paula Martins"
+mes = "Janeiro"
+valor_compra = 500
+desconto = 10
+cupom = "PAULAÉ10"
+
+
+mensagem = f"O nome da pessoa é {nome}, desconto dela é {desconto}"
+
+print(mensagem)
